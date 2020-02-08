2aee746e2e84e0749dc09fd7fc7c7b32546c7a9a
Uriel <urielchoen008@gmail.com>
2020-02-08 19:38:28 +0200

    added simple 2d game

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D Controller;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float runSpeed = 15f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate()
+    {
+        Controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+}
